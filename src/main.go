00341633b8805f1d67d7fe38086df1b3f931ab92
alrifqi <reza.rifqi@yahoo.com>
2017-01-04 22:11:52 +0700

    initial commit

---
+++ src/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+	"github.com/gorilla/mux"
+)
+
+type Note struct {
+	Title		string		`json:"title"`
+	Description	string		`json:"description"`
+	CreatedOn	time.Time 	`json:"createdon"`
+}
+
+var noteStore = make(map[string]Note)
+
+var id int = 0
+
+func PostNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var note Note
+
+	err:=json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		panic(err)
+	}
+	note.CreatedOn = time.Now()
+	id++
+	k:=strconv.Itoa(id)
+	noteStore[k]=note
+
+	j, err:=json.Marshal(note)
+	if err != nil{
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
+
+func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var notes []Note
+
+	for _, v:= range noteStore {
+		notes = append(notes, v)
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	j, err := json.Marshal(notes)
+	if err != nil{
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func GetMain(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Main Site"))
+}
+
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/", GetMain).Methods("GET")
+	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: r,	
+	}
+	log.Println("Listening....")
+	server.ListenAndServe()
+}
